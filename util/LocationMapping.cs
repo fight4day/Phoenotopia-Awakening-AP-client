828412d3fc0d25753d1c2ffb457343266933c0cd
fight4day <ti.ku10@web.de>
2026-07-25 13:39:26 +0200

    Move TODOs to corresponding place

--- util/LocationMapping.cs
+++ util/LocationMapping.cs
@@ -3528,7 +3528,7 @@ public class LocationMapping
                     "name=planto;profile=planto;voice=man,0.85;speech=PLANTO_P3;behavior=action_only;ql=SI_FALSE,PLANTO_3;floating;sort=bg_tiles,-1;talk_range=4;spacing=stand",
             },
         },
-        ["p1_frans_base"] = new List<Check>
+        ["p1_frans_base"] = new List<Check> // TODO: implement gift checks 7676512 to 7676515 + make non-missable
         {
             new Check
             {
@@ -3705,54 +3705,53 @@ public class LocationMapping
                     "profile=item,%ItemId%;speech=GEO_EXCHANGE_ARMOR;shiny;spacing=stand;talk_range=0.75;name=shop_4",
             },
         },
-        ["p1_geo_head_office"] = new List<Check>
-        {
-            new Check
-            {
-                ArchipelagoId = 7676521,
-                // ObjectIds = ["330", "340", "325"],
-                ObjectIds = ["343"],
-                IsKeyItem = true,
-                FillWhenExcluded = FillMode.Always,
-                IsNpc = false,
-                DialogReplacements = new Dictionary<int, List<string[]>>
-                {
-                    [3687] = [["Ask about earrings", "Ask about %APPlayer%%APItem%"]],
-                    [3688] = [["Ask about earrings", "Ask about %APPlayer%%APItem%"],
-                              ["Trade Antique Pin for earrings", "Trade Antique Pin for %APPlayer%%APItem%"]],
-                    [3689] = [["they", "that item"]],
-                    [3690] = [["They're supposed to be <#00ffff>lucky earrings</color>,<*_> but I don't feel any special resonance from them.", "It's supposed to be lucky <#00ffff>%APItem%</color>,<*_> but I don't feel any special resonance from it."]],
-                    [3691] = [["Do they", "Does it"]],
-                    [3692] = [["these earrings", "%APItem%"]],
-                    [3694] = [["Trade Antique Pin for earrings", "Trade Antique Pin for %APPlayer%%APItem%"]],
-                    // [3730] = [["", ""]], // TODO: how to implement conditional?
-                    [3731] = [["<#00ffff>lucky earrings</color>", "lucky <#00ffff>%APItem%</color>"],
-                              ["GO,GP8", "GO,%BonusLine%"]],
-                    [-1] = [["GIS,ITEM_remove,141,1|FILE_MARK_AP,AP_LUCKY_EARRINGS,true|black_bars_out|recycle,shelf_npc|DELAY,0.1|ANY_alpha_tween,earrings,0|AT_effect_enable,earring_halo,off|ANY_alpha_tween,earring_halo,0|particle_emitter,highlighter,stop"]],
-                },
-                GISIdentifier = "AP_LUCKY_EARRINGS",
-                OverrideType =
-                    // "name=earrings;loot=%ItemId%;sort=bg_tiles,12;use_all_bright",
-                    "name=shelf_npc;profile=!;voice=woman,1;speech=GEO_SHELF;behavior=stand;spacing=left,0.5;floating",
-            },
-            new Check
-            {
-                ArchipelagoId = 7676522,
-                ObjectIds = ["343"],
-                IsKeyItem = true,
-                FillWhenExcluded = FillMode.StatusUpgrade,
-                IsNpc = true,
-                DialogReplacements = new Dictionary<int, List<string[]>>
-                {
-                    [3730] = [["", ""]], // TODO: how to implement conditional?
-                    [3732] = [["GO,GP9", "GO,%BonusLine%"]], // TODO: can only get here via conditional in 3730
-                    [-1] = [["ITEM_remove,141,1|FILE_MARK_SI,HEART_GEO_2,true|black_bars_out"]],
-                },
-                GISIdentifier = "HEART_GEO_2",
-                OverrideType =
-                    "name=shelf_npc;profile=!;voice=woman,1;speech=GEO_SHELF;behavior=stand;spacing=left,0.5;floating",
-            },
-        },
+        // ["p1_geo_head_office"] = new List<Check> // TODO: implement 7676516 (probably needs custom level)
+        // {
+        //     new Check
+        //     {
+        //         ArchipelagoId = 7676521,
+        //         ObjectIds = ["330", "340", "325"], // Loot
+        //         // ObjectIds = ["343"], // Shelf
+        //         IsKeyItem = true,
+        //         FillWhenExcluded = FillMode.Always,
+        //         IsNpc = true,
+        //         DialogReplacements = new Dictionary<int, List<string[]>>
+        //         {
+        //             [3687] = [["Ask about earrings", "Ask about %APPlayer%%APItem%"]],
+        //             [3688] = [["Ask about earrings", "Ask about %APPlayer%%APItem%"],
+        //                       ["Trade Antique Pin for earrings", "Trade Antique Pin for %APPlayer%%APItem%"]],
+        //             [3689] = [["they", "that item"]],
+        //             [3690] = [["They're supposed to be <#00ffff>lucky earrings</color>,<*_> but I don't feel any special resonance from them.", "It's supposed to be lucky <#00ffff>%APItem%</color>,<*_> but I don't feel any special resonance from it."]],
+        //             [3691] = [["Do they", "Does it"]],
+        //             [3692] = [["these earrings", "%APItem%"]],
+        //             [3694] = [["Trade Antique Pin for earrings", "Trade Antique Pin for %APPlayer%%APItem%"]],
+        //             [3731] = [["<#00ffff>lucky earrings</color>", "lucky <#00ffff>%APItem%</color>"],
+        //                       ["GO,GP8", "GO,%BonusLine%"]],
+        //             [-1] = [["GIS,ITEM_remove,141,1|FILE_MARK_AP,AP_LUCKY_EARRINGS,true|black_bars_out|recycle,shelf_npc|DELAY,0.1|ANY_alpha_tween,earrings,0|AT_effect_enable,earring_halo,off|ANY_alpha_tween,earring_halo,0|particle_emitter,highlighter,stop"]],
+        //         },
+        //         GISIdentifier = "AP_LUCKY_EARRINGS",
+        //         OverrideType =
+        //             "name=earrings;loot=%ItemId%;sort=bg_tiles,12;use_all_bright", // Loot // TODO: showing the corresponding item doesn't work yet
+        //             // "name=shelf_npc;profile=!;voice=woman,1;speech=GEO_SHELF;behavior=stand;spacing=left,0.5;floating", // Shelf
+        //     },
+        //     new Check
+        //     {
+        //         ArchipelagoId = 7676522,
+        //         ObjectIds = ["343"],
+        //         IsKeyItem = true,
+        //         FillWhenExcluded = FillMode.StatusUpgrade,
+        //         IsNpc = true,
+        //         DialogReplacements = new Dictionary<int, List<string[]>>
+        //         {
+        //             // [3730] = [["JUMP_TO,3732,IF_TRUE|ITEM_HAVE,int_list(26)", ""]], // TODO: how to implement conditional? can only get via conditional
+        //             [3732] = [["GO,GP9", "GO,%BonusLine%"]],
+        //             [-1] = [["ITEM_remove,141,1|FILE_MARK_SI,HEART_GEO_2,true|black_bars_out"]],
+        //         },
+        //         GISIdentifier = "HEART_GEO_2",
+        //         OverrideType =
+        //             "name=shelf_npc;profile=!;voice=woman,1;speech=GEO_SHELF;behavior=stand;spacing=left,0.5;floating",
+        //     },
+        // },
         ["p1_bandit_lair_boss"] = new List<Check>
         {
             
